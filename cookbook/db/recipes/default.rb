abd0c07b7bb50845b731062392c8664e69fc8d1f
Sabirin Munye <sabirinmunye@gmail.com>
2017-09-20 10:27:55 +0100

    removed extra package

--- cookbook/db/recipes/default.rb
+++ cookbook/db/recipes/default.rb
@@ -6,18 +6,14 @@
 apt_update
 package 'mongodb'
 
-package 'mongodb' do
-	action :upgrade
-end
-
 service 'mongodb' do 
-	supports status: true, restart: true, reload: true
+	supports status: true, restart: true
 	action [:enable, :start]
 end
 
 template '/etc/mongod.conf' do
 	source 'mongo.conf.erb'
-	notifies :reload, "service[mongodb]"
+	
 end
 
 template '/lib/systemd/system/mongo.service' do
