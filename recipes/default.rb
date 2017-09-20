61543783a71c9655d4cdadcc505ac7fe1bd83ec4
Sabirin Munye <sabirinmunye@gmail.com>
2017-09-20 11:18:10 +0100

    latest version

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,13 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
 apt_update
+
+apt_repository 'mongodb-org' do 
+  uri "http://repo.mongodb.org/apt/ubuntu"
+  distribution "xenial/mongodb-org/3.2"
+  components ["multiverse"]
+  keyserver "hkp://keyserver.ubuntu.com:80"
+  key "EA312927"
+end
+
 package 'mongodb'
 
-service 'mongodb' do 
-	supports status: true, restart: true
-	action [:enable, :start]
-end
 
 template '/etc/mongod.conf' do
 	source 'mongo.conf.erb'
@@ -19,4 +25,9 @@ end
 template '/lib/systemd/system/mongo.service' do
 	source 'mongo.service.erb'
 	action :create
+end
+
+service 'mongodb' do 
+	supports status: true, restart: true
+	action [:enable, :start]
 end
